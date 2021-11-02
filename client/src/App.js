e39eda7d09c0f86274a9022631b775fd2a932727
yeeun62 <byebye62@naver.com>
2021-11-02 11:24:09 +0900

    orange msg

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,54 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import axios from "axios";
 
-function App() {
+const Wrap = styled.div`
+	text-align: center;
+	padding: 1rem;
+
+	* {
+		margin: 0;
+		padding: 0;
+		border: none;
+		outline: none;
+		list-style: none;
+		text-decoration: none;
+	}
+
+	input {
+		border: 3px solid #006495;
+		width: 15rem;
+		height: 2rem;
+		font-size: 1rem;
+		padding-left: 0.3rem;
+		margin-bottom: 1rem;
+	}
+
+	textarea {
+		border: 3px solid #006495;
+		width: 15.3rem;
+		height: 10rem;
+		resize: none;
+		margin-bottom: 0.7rem;
+	}
+
+	p {
+		font-size: 1.3rem;
+		padding: 0.4rem;
+	}
+
+	button {
+		width: 6rem;
+		height: 2rem;
+		font-size: 1rem;
+		font-weight: bold;
+		background-color: #00adc7;
+		border-radius: 0.3rem;
+		color: #fff;
+	}
+`;
+
+const App = () => {
 	const [inputChange, setInputChange] = useState({
 		phone: "",
 		msg: "",
@@ -43,11 +90,11 @@ function App() {
 	};
 
 	return (
-		<div>
+		<Wrap>
 			<p>발송자 번호</p>
 			<input
 				type="text"
-				placeholder="발송번호를 입력해주세요."
+				placeholder="-를 제외하고 입력해주세요."
 				onChange={(e) => onChange(e, "phone")}
 			/>
 			<p>메세지</p>
@@ -57,8 +104,8 @@ function App() {
 			/>
 			<br />
 			<button onClick={sendMsg}>문자 보내기</button>
-		</div>
+		</Wrap>
 	);
-}
+};
 
 export default App;
